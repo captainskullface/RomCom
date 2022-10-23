15acf48506f610b2c6fc6133ddc5d45e358d5f03
Miles <mae9775@nyu.edu>
2022-10-23 16:12:11 -0400

    Drag and drop basis
    Added two scripts  one for the dragged obj and one for where itll go

---
+++ Assets/Scripts/Emails/DropZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// this is to be put on the obj the draggedobj is going to be placed on
+
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+       if (eventData.pointerDrag != null) // if the dragged obj exists, set the dragged objs pos to this game object's pos
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
