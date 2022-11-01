cd4e76afa78e1ec64a9d235b28b5a8bbb9c6aba7
ernabalayan <erna2001balayan@gmail.com>
2022-11-01 14:56:48 -0400

    Start Screen Implemented

---
+++ Assets/Art/StartScreen/InputName.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class InputName : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputs;
+    [SerializeField] TextMeshProUGUI username;
+    [SerializeField] TextMeshProUGUI loading;
+    [SerializeField] TextMeshProUGUI hint;
+    [SerializeField] float writeWaitTime;
+    [SerializeField] float loadingTime;
+    [SerializeField] Slider loadingBar;
+    [SerializeField] float barTime;
+    bool isLoading;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputs.gameObject.SetActive(true);
+        username.gameObject.SetActive(true);
+        loading.gameObject.SetActive(false);
+        hint.gameObject.SetActive(true);
+        loadingBar.value = loadingBar.minValue;
+        loadingBar.gameObject.SetActive(false);
+        isLoading = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(loadingBar.value == loadingBar.maxValue)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (isLoading)
+        {
+            loadingBar.value += barTime * Time.deltaTime;
+        }
+    }
+    public void LoadScreen()
+    {
+        inputs.gameObject.SetActive(false);
+        username.gameObject.SetActive(false);
+        loading.gameObject.SetActive(true);
+        hint.gameObject.SetActive(false);
+        loadingBar.gameObject.SetActive(true);
+        isLoading = true;
+        StartCoroutine(BootingUp());
+
+    }
+    IEnumerator BootingUp()
+    {
+        while (true)
+        {
+            for (int i = 0; i < loading.text.Length; i++)
+            {
+                loading.maxVisibleCharacters = i + 1;
+                yield return new WaitForSeconds(writeWaitTime);
+            }
+
+            yield return new WaitForSeconds(loadingTime);
+
+            //if (loadingBar.value == loadingBar.maxValue)
+              //  break;
+        }
+    }
+
+
+
+}
